e5e7e553de50cd6c276f4956b9173947b85d67ac
Mohammad Shiwani <47725464+Zshiwani@users.noreply.github.com>
2019-10-01 23:48:18 -0500

    Initial Challenge

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,31 @@
+// from data.js
+var tableData = data;
+
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+
+data.forEach((tableData) => {
+    // console.log(tableData);
+    var row = tbody.append("tr");
+    Object.entries(tableData).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var datebtn = d3.select("#filter-btn");
+
+datebtn.on("click", function() {
+    d3.event.preventDefault();
+    tbody.html("");
+    var inputDateId = d3.select("#datetime").property("value");
+    var filterData = tableData.filter(date => date.datetime === inputDateId);
+    filterData.forEach(itInstance => {
+        var row = tbody.append(`tr`);
+        Object.entries(itInstance).forEach(([key, value]) => {
+            var cell = row.append(`td`);
+            cell.text(value);
+        });
+    });
+});
